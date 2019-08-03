5899e0794c953732d0be91da81d61bb42228e42b
Rachel T <rachmaybe@gmail.com>
2019-08-03 21:32:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,7 +42,7 @@ def using_delete (instructors, no_offense_steven)
   instructors.delete("Steven")
 end 
 
-def using_delete_at (famous_robots, deleted_robot)
+def using_delete_at (famous_robots, 2)
   famous_robots.de
 end 
 
