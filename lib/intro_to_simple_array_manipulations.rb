6fa3ddf4e840e3d9eac21b15eeeb9e2c2a66ec12
Rachel T <rachmaybe@gmail.com>
2019-08-03 20:43:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -16,7 +16,7 @@ end
 def shift_with_args (ice_cream_brands)
   ice_cream_brands.shift(2)
 end 
-def using_concat (my_favorite_things, more)
+def using_concat (my_favorite_things, more_favs)
   
 end 
 def using_insert
