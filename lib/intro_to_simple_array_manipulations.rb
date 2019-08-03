892c75219c98820d89a0c58f812d09822dca1b0d
Rachel T <rachmaybe@gmail.com>
2019-08-03 20:37:33 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,31 @@
+def using_push (a,b)
+a.push(b)
+end
+def using_unshift (a,b)
+  a.unshift(b)
+end 
+def using_pop (countries)
+  countries.pop
+end 
+def pop_with_args (dog_breeds)
+  dog_breeds.pop(2)
+end
+def using_shift (my_favorite_cities)
+  my_favorite_cities.shift
+end 
+def shift_with_args (ice_cream_brands)
+  ice_cream_brands.shift(2)
+end 
+def using_concat (my_favorite_things)
+  
+end 
+def using_insert
+end 
+def using_uniq
+end 
+def using_flatten
+end 
+def using_delete
+end 
+def using_delete_at
+end 
