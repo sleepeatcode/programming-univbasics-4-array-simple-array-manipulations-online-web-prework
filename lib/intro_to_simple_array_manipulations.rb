325c581d15e82d721490c469845e0d97b965541b
Rachel T <rachmaybe@gmail.com>
2019-08-03 20:47:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -20,7 +20,7 @@ def using_concat (my_favorite_things, more_favs)
   my_favorite_things.concat(more_favs)
 end 
 def using_insert (list_of_programming_languages, another_language)
-  list_of_programming_languages.insert(1)
+  list_of_programming_languages.insert(5)
 end 
 def using_uniq
 end 
